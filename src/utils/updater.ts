38b7f7234eeeeed6e2fb0a7fe97913abe71ad045
Pakwan Manuel Marquez <barreramarquez2003@gmail.com>
2026-09-14 02:55:19 +0800

    chore(release): v1.0.3

--- src/utils/updater.ts
+++ src/utils/updater.ts
@@ -24,7 +24,7 @@ export async function checkForUpdate(): Promise<AppUpdateInfo> {
   if (!isTauri()) {
     return {
       available: false,
-      currentVersion: '1.0.2',
+      currentVersion: '1.0.3',
     };
   }
 
@@ -33,7 +33,7 @@ export async function checkForUpdate(): Promise<AppUpdateInfo> {
     if (!update) {
       return {
         available: false,
-        currentVersion: '1.0.2',
+        currentVersion: '1.0.3',
       };
     }
 
